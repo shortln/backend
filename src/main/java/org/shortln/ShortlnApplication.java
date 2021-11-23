19b8381337a7423ab19bf3990964a676384bd92d
yijie4188 <yijie4188@gmail.com>
2021-11-23 20:08:11 +0800

    feat: init

---
+++ src/main/java/org/shortln/ShortlnApplication.java
@@ -0,0 +1,12 @@
+package org.shortln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortlnApplication.class, args);
+    }
+}
